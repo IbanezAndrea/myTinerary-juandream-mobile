29a8418247f52bf6296f31209384b2cf44d31a01
Ibañez Andrea <108688594+IbanezAndrea@users.noreply.github.com>
2022-09-27 14:27:23 -0300

    fix 1

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import 'react-native-gesture-handler'; 
 import React from 'react';
 import Drawer from './src/navigation/Drawer';
-import { NavigationContainer } from '@react-navigation/native/lib/commonjs';
-
-
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { createDrawerNavigator } from '@react-navigation/drawer/lib/commonjs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from '../screens/HomeScreen'
+import Cities from '../screens/Cities'
 
 const DrawerNavigation = createDrawerNavigator()
 
@@ -8,6 +9,7 @@ export default function Drawer() {
     return (
     <DrawerNavigation.Navigator>
         <DrawerNavigation.Screen name ='Home' component={HomeScreen}/>
+        <DrawerNavigation.Screen name ='Cities' component={Cities}/>
     </DrawerNavigation.Navigator>
     )
 }
