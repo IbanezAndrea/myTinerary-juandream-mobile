f92712ede2035f9d562f499cc21cb061965505f5
juanPedroGalarza <juanpg232@gmail.com>
2022-09-28 16:20:04 -0300

    redux update

---
+++ src/features/actions/usersAPI.js
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import apiurl from "../../api";
+
+
+export const usersAPI = createApi({
+    reducerPath: "usersAPI",
+    baseQuery: fetchBaseQuery({
+        baseUrl: apiurl
+        }),
+        endpoints: (builder) =>({
+        userSignUp: builder.mutation({
+            query: (user) =>({
+            url: "/auth/signup",
+            method: "POST",
+            body: user
+                }),
+        }),
+        userSignIn: builder.mutation({
+            query: (user) =>({
+                url: "/auth/signin",
+                method: "POST",
+                body: user
+            }), 
+            }),
+        userSignOut: builder.mutation({
+            query: ({user,token}) => ({
+                url: "/auth/signout",
+                method: "POST",
+                body: user,
+                headers: {"Authorization": "Bearer " + token}
+            })
+        }),
+        verifyToken: builder.mutation({
+            query: (token)=>({
+                url: "/auth/token",
+                headers: {"Authorization": "Bearer " + token}
+                })
+            }),
+            editUser: builder.mutation({
+                query: (data) =>({
+                    url: `/auth/`,
+                    method:"PUT",
+                    body:data,
+                    headers: { "Authorization": "Bearer " + localStorage.getItem("token")}
+                })
+            }),
+            getUser: builder.query({
+                query: (id) => `/auth/${id}`
+            })
+        })
+    })
+
+    export const { 
+            useUserSignUpMutation,
+            useUserSignInMutation,
+            useUserSignOutMutation,
+            useVerifyTokenMutation,
+            useEditUserMutation,
+            useGetUserQuery
+        } = usersAPI
