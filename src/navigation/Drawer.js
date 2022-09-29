c5e9fec4cd66bacd23c20fe177854e63bd05b3d7
juanPedroGalarza <juanpg232@gmail.com>
2022-09-28 22:20:58 -0300

    route city drawer

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -4,21 +4,19 @@ import HomeScreen from '../screens/HomeScreen'
 import Cities from '../screens/Cities'
 import { StyleSheet } from 'react-native'
 import SignIn from "../screens/SignIn"
-
+import City from '../screens/City'
 const DrawerNavigation = createDrawerNavigator()
 
 export default function Drawer() {
     return (
-    <DrawerNavigation.Navigator>
-        <DrawerNavigation.Screen name ='Home' component={HomeScreen}/>
-        <DrawerNavigation.Screen name ='Cities' component={Cities}/>
-        <DrawerNavigation.Screen name ='Sign in' component={SignIn}/>
-    </DrawerNavigation.Navigator>
+        <DrawerNavigation.Navigator>
+            <DrawerNavigation.Group>
+                <DrawerNavigation.Screen name='Home' component={HomeScreen} />
+                <DrawerNavigation.Screen name='Cities' component={Cities} />
+                <DrawerNavigation.Screen name='Sign in' component={SignIn} />
+            </DrawerNavigation.Group>
+            <DrawerNavigation.Screen name='City' component={City}
+                initialParams={{ id: "" }} />
+        </DrawerNavigation.Navigator>
     )
 }
-
-const styles = StyleSheet.create({
-
-
-
-})
